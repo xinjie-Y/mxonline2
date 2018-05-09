a7561b6db4b7c7fc4f2dcd093a3030f12006cdf9
xinjie <2314388662@qq.com>
2018-05-09 11:29:35 +0800

    ‘m=-m登陆注册完成

--- apps/users/views.py
+++ apps/users/views.py
@@ -9,6 +9,7 @@ from django.views.generic.base import View
 from .forms import LoginForm,RegisterForm
 from django.contrib.auth.hashers import make_password
 
+
 # 实现用户名邮箱均可登录
 # 继承ModelBackend类，因为它有方法authenticate，可点进源码查看
 class CustomBackend(ModelBackend):
@@ -65,19 +66,26 @@ class LoginView(View):
                     "login_form": login_form})
 
 
-# 注册功能的view
 class RegisterView(View):
     # get方法直接返回页面
     def get(self, request):
         # 添加验证码
         register_form = RegisterForm()
-        return render(request, "register.html", {'register_form':register_form})
+        return render(
+            request, "register.html", {
+                'register_form': register_form})
 
     def post(self, request):
         # 实例化form
         register_form = RegisterForm(request.POST)
         if register_form.is_valid():
+            # 这里注册时前端的name为email
             user_name = request.POST.get("email", "")
+            # 用户查重
+            if UserProfile.objects.filter(email=user_name):
+                return render(
+                    request, "register.html", {
+                        "register_form": register_form, "msg": "用户已存在"})
             pass_word = request.POST.get("password", "")
 
             # 实例化一个user_profile对象，将前台值存入
@@ -85,7 +93,25 @@ class RegisterView(View):
             user_profile.username = user_name
             user_profile.email = user_name
 
+            # 默认激活状态为false
+            user_profile.is_active = False
+
             # 加密password进行保存
             user_profile.password = make_password(pass_word)
             user_profile.save()
-            pass
+
+            # # 写入欢迎注册消息
+            # user_message = UserMessage()
+            # user_message.user = user_profile.id
+            # user_message.message = "欢迎注册mtianyan慕课小站!! --系统自动消息"
+            # user_message.save()
+            # # 发送注册激活邮件
+            # send_register_eamil(user_name, "register")
+
+            # 跳转到登录页面
+            return render(request, "login.html", )
+        # 注册邮箱form验证失败
+        else:
+            return render(
+                request, "register.html", {
+                    "register_form": register_form})
